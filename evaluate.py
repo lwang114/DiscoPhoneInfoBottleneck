f1c9aaf1aea476224570e76928e38ef9659c4c7a
Liming Wang <lwang114@illinois.edu>
2021-02-07 23:26:08 -0600

    evaluator

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,8 @@
 import argparse
-import utils
 import pandas as pd
+import numpy as np
 
+EPS = 1e-40
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Evaluate the discovered phone units.'
@@ -10,21 +11,26 @@ def parse_args():
         '--config', type=str, help=''
     )
 
-def evaluate(pred_path, gold_path, token_path):
+def evaluate(pred_dicts, gold_dicts, token_path=None, ds_rate=1):
   '''
   Evaluate the predictions of a phoneme discovery system
   Args:
-      pred_path (str) : Filename of a list of mappings
+      pred_dicts (list) : A list of mappings
           {'sentence_id' : str,
            'units' : a list of ints representing cluster id for each feature frame}
-      gold_path (str) : Filename of a list of mappings
+      gold_dicts (list) : A list of mappings
           {'sentence_id': str,
            'units' : a list of ints representing phoneme id for each feature frame,
            'text' : a list of strs representing phoneme tokens for each feature frame}       
   '''
-  pred_dicts = json.load(open(pred_path, 'r'))
-  gold_dicts = json.load(open(gold_path, 'r'))
-  token_to_index = json.load(open(token_path, 'r'))
+  if token_path is None:
+    token_to_index = {}
+    for gold_dict in gold_dicts:
+      for g_idx, g_token in zip(gold_dict['units'], gold_dict['text']):
+        if not g_token in token_to_index:
+          token_to_index[g_token] = g_idx
+  else:
+    token_to_index = json.load(open(token_path, 'r'))
   tokens = sorted(token_to_index)
   n_gold_tokens = len(tokens)
 
@@ -33,33 +39,31 @@ def evaluate(pred_path, gold_path, token_path):
 
   # Compute coocurrence and confusion matrix
   confusion_dict = {t:np.zeros((n_pred_tokens,)) for t in tokens}
-  confusion_mat = np.zeros((n_pred_tokens, n_gold_tokens))
+  confusion_mat = np.zeros((n_gold_tokens, n_pred_tokens))
   for pred_dict, gold_dict in zip(pred_dicts, gold_dicts):
-    for p_idx, g_idx, g_token in zip(pred_dict['units'], gold_dict['units'], gold_dict['text']):
+    gold_units = gold_dict['units'][::ds_rate]
+    gold_tokens = gold_dict['text'][::ds_rate]
+    for p_idx, g_idx, g_token in zip(pred_dict['units'], gold_units, gold_tokens):
       confusion_dict[g_token][p_idx] += 1
       confusion_mat[g_idx][p_idx] += 1
-
 
   # Compute token F1
   token_recall = 0. 
   token_precision = 0.
-  for g_token in confusion_dict:
-    confusion_dict[g_token] /= confusion_dict[g_token].sum()
     
   for g_idx in range(n_gold_tokens):
-    token_recall += max(confusion_dict[g_idx]) / sum(confusion_dict[g_idx])
+    token_recall += max(confusion_mat[g_idx]) / max(sum(confusion_mat[g_idx]), EPS)
   token_recall /= n_gold_tokens
 
   for p_idx in range(n_pred_tokens):
-    token_precision += max(confusion_dict[:, p_idx]) / sum(confusion_dict[:, p_idx])
+    token_precision += max(confusion_mat[:, p_idx]) / max(sum(confusion_mat[:, p_idx]), EPS)
   token_precision /= n_pred_tokens
 
   token_f1 = 2 * token_precision * token_recall / (token_precision + token_recall) if (token_precision + token_recall) > 0 else 0
 
   print('Token precision={:.3f}\tToken recall={:.3f}\tToken F1={:.3f}'.format(token_precision, token_recall, token_f1))  
   confusion_df = pd.DataFrame(confusion_dict)
-  confusion_df.to_csv(pred_path+'_confusion_matrix.csv')
-
+  return token_f1, confusion_df
 
 def main():
   args = parse_args()
