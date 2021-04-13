303e7326b5bbe4327bd99a542560c43257602151
Liming Wang <lwang114@illinois.edu>
2021-04-13 11:43:52 -0500

    RCNN features by matching bounding boxes

---
+++ bottom-up-features/extract_features_flickr.py
@@ -0,0 +1,69 @@
+import os
+import numpy as np
+
+def IoU(b1, b2):
+  x_minmin, x_minmax = min(b1[0], b2[0]), max(b1[0], b2[0])
+  y_minmin, y_minmax = min(b1[1], b2[1]), max(b1[1], b2[1])
+  x_maxmin, x_maxmax = min(b1[2], b2[2]), max(b1[2], b2[2])
+  y_maxmin, y_maxmax = min(b1[3], b2[3]), max(b1[3], b2[3])
+
+  if x_minmax >= x_maxmin or y_minmax >= y_maxmin:
+    return 0
+
+  Si = (x_maxmin - x_minmax) * (y_maxmin - y_minmax)
+  So = (x_maxmax - x_minmin) * (y_maxmax - y_minmin)
+  return Si / So  
+
+def find_best_match_box(gt_box, pred_boxes, thres=0.5):
+  ious = [IoU(gt_box, p_box) for p_box in pred_boxes]
+  best_idx = np.argmax(ious)
+  if ious[best_idx] < thres:
+    print(f'Best box IoU {ious[best_idx]} does not reach the IoU threshold {thres}')
+    return -1
+  return best_idx
+
+
+def main():
+  data_path = '/home/lwang114/data/flickr'
+  rcnn_box_f = open(os.path.join(data_path, 'flickr8k_rcnn_bboxes.json'), 'w')
+  feat_to_boxes = dict()
+  for line in rcnn_box_f:
+    parts = line.rstrip('\n').split()
+    feat_id = parts[0]
+    box = [float(x) for x in parts]
+    if not feat_id in feat_to_boxes:
+      feat_to_boxes[feat_id] = [box]
+    else:
+      feat_to_boxes[feat_id].append(box)
+
+  phrase_f = open(os.path.join(data_path, 'flickr8k_phrases.json'), 'r')
+  out_f = open(os.path.join(data_path, 'flickr8k_phrases_rcnn_feats.json', 'w'))
+  idx = 0
+  keep_idx = 0
+  for line in phrase_f:
+    phrase = json.loads(line)
+    idx += 1
+    if idx > 50: # XXX
+      break
+    gt_box = phrase['bbox']
+    utt_id = phrase['utterance_id']
+    print(idx, utt_id)
+    image_id = '_'.join(utt_id.split('_')[:-1])
+    capt_id = utt_id.split('_')[-1]
+    feat_id = '_'.join([image_id+'.jpg', capt_id])
+    pred_boxes = feat_to_boxes[feat_id] 
+
+    # For each groundtruth box, find the predicted box closest to it and
+    # use its feature if IoU(gt_box, pred_box) > 0.5
+    best_box_idx = find_best_match_box(gt_box, pred_boxes)
+    if best_box_idx >= 0:
+      keep_idx += 1
+      phrase['rcnn_box'] = pred_boxes[best_box_idx]
+      phrase['rcnn_feat_id'] = best_box_idx
+    out_f.write(json.dumps(phrase)+'\n')
+  print(f'Find RCNN boxes for {keep_idx} out of {idx} ground truth boxes')
+  phrase_f.close()
+
+
+  
+
