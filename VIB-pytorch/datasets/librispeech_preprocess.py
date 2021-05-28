38aa97d8bf61828f81ebce18f288fd089533f646
Liming Wang <lwang114@ifp-10.ifp.uiuc.edu>
2021-05-28 18:00:51 -0500

    starter code for librispeech

---
+++ VIB-pytorch/datasets/librispeech_preprocess.py
@@ -0,0 +1,14 @@
+def extract_visual_words(data_path, visual_word_file):
+  """
+  Parameters:
+  ----------
+    data_path: str, path to the LibriSpeech root
+        transcription format: {dir1-dir2-sent_id} {word 1 in cap} ... {word last in cap}  
+    visual_word_file: str, filename storing the dictionary of visual words 
+
+  Returns:
+  -------
+    
+
+  """
+  
