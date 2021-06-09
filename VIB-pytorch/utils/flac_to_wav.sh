434edca3eaaf9c0c2500a39fe736d46484dcfaf5
Liming Wang <lwang114@illinois.edu>
2021-06-08 20:11:16 -0500

    convert flac to wav for librispeech

--- VIB-pytorch/utils/flac_to_wav.sh
+++ VIB-pytorch/utils/flac_to_wav.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 data_root=/ws/ifp-53_2/hasegawa/lwang114/data/LibriSpeech
-out_dir=/ws/ifp-53_2/hasegawa/lwang114/data/LibriSpeech/wavs
+out_dir=/ws/ifp-53_2/hasegawa/lwang114/data/zerospeech-dataset
 mkdir -p ${out_dir}
 
-for split in train-clean-100 dev-clean-100; do
+for split in train-clean-100; do
   mkdir -p ${out_dir}/${split}
 done
 
@@ -13,7 +13,7 @@ for dr1 in ${data_root}/${split}; do
     for fn in ${data_root}/${split}/${dr1}/${dr2}; do
       prefix=${fn#.flac}
       echo ${prefix}
-      ffmpeg -i ${data_root}/${split}/${dr1}/${dr2}/${fn} ${data_root}/${split}/${dr1}/${dr2}/${prefix}.wav # TODO 
+      ffmpeg -i ${data_root}/${split}/${dr1}/${dr2}/${fn} ${out_dir}/${split}/${prefix}.wav # TODO 
     done
   done
 done
