bf424e50c5fd8afbad9a0b2b9c3f1934d1bb89ee
Liming Wang <lwang114@ifp-10.ifp.uiuc.edu>
2021-06-20 09:29:50 -0500

    new evaluation metrics

--- VIB-pytorch/utils/evaluate.sh
+++ VIB-pytorch/utils/evaluate.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 # . ./parse_options.sh
-DATA_ROOT=../../../../data/flickr30k/flickr8k_word_50 
-EXP_DIR=../checkpoints/phone_discovery_visual_label_gumbel_mlp/ 
-while getopts ":e:d:" arg; do
+DATA_ROOT=../../../../data/zerospeech2021-dataset/phonetic
+EXP_DIR=../checkpoints/phone_discovery_librispeech_460h_wav2vec2_real
+while getopts ":e:d:f:" arg; do
   case $arg in
     e) EXP_DIR=$OPTARG;;
     d) DATA_ROOT=$OPTARG;; 
+    f) FRAME_RATE=$OPTARG;;
   esac
 done
 
@@ -20,8 +21,21 @@ for x in phonetic lexical syntactic semantic; do
   mkdir -p ${EXP_DIR}/outputs/$x/dev-other
 done
 
-cp ../configs/meta.yaml ${EXP_DIR}/outputs
-cp ${DATA_ROOT}/test/test_400.item ${EXP_DIR}/inputs/phonetic/dev-clean/dev-clean.item
-cp ${DATA_ROOT}/test/test_400.item ${EXP_DIR}/inputs/phonetic/dev-other/dev-other.item
+cp ../configs/meta_${FRAME_RATE}ms.yaml ${EXP_DIR}/outputs/meta.yaml
+if [[ -f ${DATA_ROOT}/dev-clean/dev-clean.item ]]; then
+  echo "dev-clean.item found !"
+  cp ${DATA_ROOT}/dev-clean/dev-clean.item ${EXP_DIR}/inputs/phonetic/dev-clean/dev-clean.item
+else
+  cp ${DATA_ROOT}/test/test.item ${EXP_DIR}/inputs/phonetic/dev-clean/dev-clean.item
+fi
+
+
+if [[ -f ${DATA_ROOT}/dev-other/dev-other.item ]]; then
+  echo "dev-other.item found !"
+  cp ${DATA_ROOT}/dev-clean/dev-clean.item ${EXP_DIR}/inputs/phonetic/dev-other/dev-other.item
+else
+  cp ${DATA_ROOT}/test/test.item ${EXP_DIR}/inputs/phonetic/dev-other/dev-other.item
+fi
+
 # zerospeech2021-validate ${EXP_DIR}/inputs/ ${EXP_DIR}/outputs/ --no-lexical --no-syntactic --no-semantic --only-dev
-zerospeech2021-evaluate ${EXP_DIR}/inputs ${EXP_DIR}/outputs --no-lexical --no-syntactic --no-semantic -o ${EXP_DIR}/results
+zerospeech2021-evaluate ${EXP_DIR}/inputs ${EXP_DIR}/outputs --no-lexical --no-syntactic --no-semantic -o ${EXP_DIR}/results --force-cpu
