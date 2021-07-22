d6042285077b9155d99a857f64627e70edddd52f
Liming Wang <lwang114@illinois.edu>
2021-07-22 15:06:12 -0500

    dib

---
+++ VIB-pytorch/probabilistic_clustering.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class DIB: # TODO
+  """ 
+  Deterministic information bottleneck as described in
+
+      D. Strouse & D. Schwab, "The deterministic information bottleneck". UAI 2015
+
+  """
+  def __init__(self, n_clusters):
+    self.n_clusters = n_clusters
+    self.cluster_centers_ = None
+
+  def fit(self, X):
+    pass
+
+  def predict(self, X):
+    pass
+
+  def dib_plusplus_(self, X):
+    pass
+   
